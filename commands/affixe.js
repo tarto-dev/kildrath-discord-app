b67641e1f5736fb7b8f25a97849853cc68a398f1
Benftwc <benftwc@gmail.com>
2019-08-05 16:42:46 +0000

    Update affixe.js

--- commands/affixe.js
+++ commands/affixe.js
@@ -11,20 +11,20 @@ exports.run = (client, message, args) => {
             "color": 9732809,
             "fields": [
                 {
-                    "name": affixes[0].name,
-                    "value": affixes[0].description + `[wowhead](${affixes[0].wowhead_url})`
+                    "name": `[${affixes[0].name}](${affixes[0].wowhead_url})`,
+                    "value": affixes[0].description
                 },
                 {
-                    "name": affixes[1].name,
-                    "value": affixes[1].description + `[wowhead](${affixes[1].wowhead_url})`
+                    "name": `[${affixes[1].name}](${affixes[1].wowhead_url})`,
+                    "value": affixes[1].description
                 },
                 {
-                    "name": affixes[2].name,
-                    "value": affixes[2].description + `[wowhead](${affixes[2].wowhead_url})`
+                    "name": `[${affixes[2].name}](${affixes[2].wowhead_url})`,
+                    "value": affixes[2].description
                 },
                 {
-                    "name": affixes[3].name,
-                    "value": affixes[3].description + `[wowhead](${affixes[3].wowhead_url})`
+                    "name": `[${affixes[3].name}](${affixes[3].wowhead_url})`,
+                    "value": affixes[3].description
                 },
             ]
         };
