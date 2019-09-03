21a731cfe7e0985b9cbf4fa3ca114790acd04d0b
Benftwc <benftwc@gmail.com>
2019-09-03 20:12:59 +0000

    Passage au FR du bot PETS

--- commands/pets.js
+++ commands/pets.js
@@ -5,5 +5,5 @@ exports.run = (client, message, args) => {
     }
     
     const [nickname, realm = "hyjal", region = "eu", ...rest] = args;
-    message.channel.send(`https://wow-pets.com/character/${region}/${realm}/${nickname}`);
+    message.channel.send(`https://fr.wow-pets.com/character/${region}/${realm}/${nickname}`);
 }
