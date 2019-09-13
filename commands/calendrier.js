3a766dfd2e71e4358c255006fb036170a35ef480
Benftwc <benftwc@gmail.com>
2019-09-13 22:08:28 +0000

    Delete calendrier.js

--- commands/calendrier.js
+++
@@ -1,5 +0,0 @@
-exports.run = (client, message, args) => {
-    message.delete();
-    message.channel.send(`Commande en cour de réparation :ok_hand:`);
-    //message.channel.send(`Derniers runs de la guilde dispo sur https://www.warcraftlogs.com/guild/calendar/345414/`);
-}
