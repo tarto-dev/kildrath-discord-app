bcf53877a3654d497e14d279f079e6f2f3efbd32
Benftwc <benftwc@gmail.com>
2019-09-13 22:11:59 +0000

    Update simc.js

--- commands/simc.js
+++ commands/simc.js
@@ -1,4 +1,5 @@
 exports.run = (client, message, args) => {
     const [nickname, realm = "hyjal", region = "eu", ...rest] = args;
-    message.channel.send(`https://www.raidbots.com/simbot/quick?region=${region}&realm=${realm}&name=${nickname}`)
+    message.channel.send(`https://www.raidbots.com/simbot/quick?region=${region}&realm=${realm}&name=${nickname}`);
+    message.react('👌');
 }
