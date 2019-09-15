60068c3e222bca79b2786b27ecc0dc21a195ada1
Benftwc <benftwc@gmail.com>
2019-09-15 14:14:56 +0000

    Update pets.js

--- commands/pets.js
+++ commands/pets.js
@@ -5,6 +5,7 @@ exports.run = (client, message, args) => {
     }
     
     const [nickname, realm = "hyjal", region = "eu", ...rest] = args;
-    message.channel.send(`https://fr.wow-pets.com/character/${region}/${realm}/${nickname}`);
+    //message.channel.send(`https://fr.wow-pets.com/character/${region}/${realm}/${nickname}`);
+    message.channel.send(`Site en maintenance - alternative : https://magpie.wow-petguide.com`);
     message.react('👌');
 }
