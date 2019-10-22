fb29e148937491d1277af8b923d51717e69fca41
Benftwc <benftwc@gmail.com>
2019-10-22 12:14:09 +0000

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
-    message.author.sendMessage(`Si t 'as besoin d'aide pour configurer ou faire un retour sur le bot, je t'invite à rejoindre le discord de support, accessible via http://disboard.org/fr/server/485527398557810695.`);
+    message.channel.send(`Si t 'as besoin d'aide pour configurer ou faire un retour sur le bot, je t'invite à rejoindre le discord de support, accessible via http://disboard.org/fr/server/485527398557810695.`);
+    message.react('👌');
 }
