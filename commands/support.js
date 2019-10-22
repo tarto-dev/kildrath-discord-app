4c0fdeed908ab95afda56124ff26fe29f1a4900f
Benftwc <benftwc@gmail.com>
2019-10-22 11:54:27 +0000

    Add new file

---
+++ commands/support.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.author.sendMessage(`Si t 'as besoin d'aide pour configurer ou faire un retour sur le bot, je t'invite à rejoindre le discord de support, accessible via http://disboard.org/fr/server/485527398557810695.`);
+}
