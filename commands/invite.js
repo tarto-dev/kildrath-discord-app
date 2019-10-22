8b12ab2e93d3d167aed6cf6365f10c77d235f973
Benftwc <benftwc@gmail.com>
2019-10-22 12:16:34 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
-    message.author.sendMessage(`Tu peux ajouter Kildrath chez toi en cliquant sur https://discordapp.com/api/oauth2/authorize?client_id=500684708888444951&permissions=8&scope=bot :smirk:.`);
+    message.channel.send(`Tu peux ajouter Kildrath chez toi en cliquant sur https://discordapp.com/api/oauth2/authorize?client_id=500684708888444951&permissions=8&scope=bot :smirk:.`);
+    message.react('👌');
 }
