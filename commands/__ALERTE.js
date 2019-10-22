a1aed9b62a458d409272c60d7717ae6c17e5270c
Benftwc <benftwc@gmail.com>
2019-10-22 10:03:22 +0000

    Update __ALERTE.js

--- commands/__ALERTE.js
+++ commands/__ALERTE.js
@@ -5,11 +5,8 @@ exports.run = async (client, message, args) => {
     message.delete();
 
     client.guilds.map(guild => {
-        const guildSet = client.settings.get(guild.id);
-        if(guildSet.hasOwnProperty('annonces') && guildSet.annonces === "on") {
-            let notif_chan = guild.channels.find(channel => channel.name === ("general" || "général" || "accueil" || "annonce" || "annonces"));
-            notif_chan.send(""+args.join(' '))
-        }
+        let notif_chan = guild.channels.find(channel => channel.name === ("general" || "général" || "accueil" || "annonce" || "annonces"));
+        notif_chan.send(""+args.join(' '))
     });
 
     message.channel.send(`Message diffusé sur ${client.guilds.size} serveurs \n\n\n ${args.join(' ')}`);}
