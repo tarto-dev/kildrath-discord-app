0cb775c5a66ebf5b8f3d5c877f93409f014b6a72
Benftwc <benftwc@gmail.com>
2020-06-20 18:27:44 +0000

    Update scan.js - Now uses Simplearmory

--- commands/scan.js
+++ commands/scan.js
@@ -1,38 +1,5 @@
 exports.run = async (client, message, args) => {
-    //message.delete();
-    const [nickname, realm = "Hyjal", region = "EU", ...rest] = args;
-
-    const embed = {
-        "title": `Qui est ${nickname} :thinking:`,
-        "color": 4886754,
-        "fields": [
-            {
-                "name": ":shield: Armory",
-                "value": `https://worldofwarcraft.com/fr-fr/character/${realm}/${nickname}`
-            },
-            {
-                "name": ":mag: Wowanalyser",
-                "value": `https://wowanalyzer.com/character/${region}/${realm}/${nickname}/`
-            },
-            {
-                "name": ":gear: Raider.io",
-                "value": `https://raider.io/characters/${region.toLowerCase()}/${realm.toLowerCase()}/${nickname}`
-            },
-            {
-                "name": ":robot: Simc / Raidbot",
-                "value": `https://www.raidbots.com/simbot/quick?region=${region.toLowerCase()}&realm=${realm.toLowerCase()}&name=${nickname}`
-            },
-            {
-                "name": ":crossed_swords: Wipefest",
-                "value": `https://www.wipefest.net/character/${nickname}/${realm}/${region}`
-            },
-            {
-                "name": ":dog: WoW Pets",
-                "value": `Site en maintenance - alternative : https://magpie.wow-petguide.com`
-            }
-        ]
-    };
-
-    message.channel.send({embed});
+    const [nickname, realm = "hyjal", region = "eu", ...rest] = args;
+    message.channel.send(`https://simplearmory.com/#/${region}/${realm}/${nickname}`);
     message.react('👌');
 };
